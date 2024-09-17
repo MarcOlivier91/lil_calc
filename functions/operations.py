e0dec0f54efd1caaa9280e8290e244a3ca97fd3c
MarcOlivier91 <smarc.mailpro@gmail.com>
2024-09-17 19:11:08 +0200

    fixed exit condition

--- functions/operations.py
+++ functions/operations.py
@@ -8,32 +8,26 @@ def operations() :
     os.system('cls')
     a = int(input(colored("Choose your first number : ", "white", "on_blue")))
     b = int(input(colored("Choose your second number : ", "white", "on_blue")))
-    print(a)    
-    print(b)
-    while operationChoice != 9:
-        if operationChoice == 1 :
-            result = int(a) + int(b)
-            print(f"The result is {result}")
+    
+    if operationChoice == 9:
+        print("Bye")
+
+    elif operationChoice == 1 :
+        result = int(a) + int(b)
+        print(f"The result is {result}")
+    
+    elif operationChoice == 2 :
+        result = int(a) - int(b)
+        print(f"The result is {result}")
+    
+    elif operationChoice == 3 :
+        result = int(a) * int(b)
+        print(f"The result is {result}")
         
-        elif operationChoice == 2 :
-            result = int(a) - int(b)
+    elif operationChoice == 4 :
+        if int(a) or int(b) != 0 :                
+            result = int(a) / int(b)
             print(f"The result is {result}")
-        
-        elif operationChoice == 3 :
-            result = int(a) * int(b)
-            print(f"The result is {result}")
-            
-        elif operationChoice == 4 :
-            if int(a) or int(b) != 0 :                
-                result = int(a) / int(b)
-                print(f"The result is {result}")
-            else:                
-                print(colored("You cannot divide by zero !", "black", "on_red"))
+        else:                
+            print(colored("You cannot divide by zero !", "black", "on_red"))
 
-
-        sleep(3)
-        os.system('cls')
-        inputs()
-    operationChoice = input()
-
-    print("Bye")
