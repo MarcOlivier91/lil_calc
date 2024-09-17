4d0294e95cb7894645233b7c57423e73927c9ac6
MarcOlivier91 <smarc.mailpro@gmail.com>
2024-09-17 20:03:45 +0200

    deleted unnecessary files

--- functions/temp.py
+++
@@ -1,4 +0,0 @@
-    print(colored("[A] Add  ", "black", 'on_white'))
-    print(colored("[B] Substract  ", "black", "on_white"))
-    print(colored("[C] Multiply  ", "black", "on_white"))
-    print(colored("[D] Divide  ","black", "on_white"))
