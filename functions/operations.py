ee439013a3a6a91a5862c001b5452f6d1f951b0d
MarcOlivier91 <smarc.mailpro@gmail.com>
2024-09-17 19:19:08 +0200

    switched from if/else to match case

--- functions/operations.py
+++ functions/operations.py
@@ -6,28 +6,29 @@ from functions.inputs import *
 def operations() :     
     operationChoice = input()
     os.system('cls')
-    a = int(input(colored("Choose your first number : ", "white", "on_blue")))
-    b = int(input(colored("Choose your second number : ", "white", "on_blue")))
+    # a = int(input(colored("Choose your first number : ", "white", "on_blue")))
+    # b = int(input(colored("Choose your second number : ", "white", "on_blue")))
     
     if operationChoice == 9:
         print("Bye")
 
-    elif operationChoice == 1 :
-        result = int(a) + int(b)
-        print(f"The result is {result}")
-    
-    elif operationChoice == 2 :
-        result = int(a) - int(b)
-        print(f"The result is {result}")
-    
-    elif operationChoice == 3 :
-        result = int(a) * int(b)
-        print(f"The result is {result}")
-        
-    elif operationChoice == 4 :
-        if int(a) or int(b) != 0 :                
-            result = int(a) / int(b)
+    match operationChoice:
+        case 1:
+            result = int(a) + int(b)
             print(f"The result is {result}")
-        else:                
-            print(colored("You cannot divide by zero !", "black", "on_red"))
+    
+        case 2 :
+            result = int(a) - int(b)
+            print(f"The result is {result}")
+    
+        case 3 :
+            result = int(a) * int(b)
+            print(f"The result is {result}")
+        
+        case 4 :
+            if int(a) or int(b) != 0 :                
+                result = int(a) / int(b)
+                print(f"The result is {result}")
+            else:                
+                print(colored("You cannot divide by zero !", "black", "on_red"))
 
